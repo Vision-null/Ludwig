a795adda3236bf5f0947988c700d702ead27a964
Saagar Mehta <saagar.mehta97@gmail.com>
2024-07-09 20:36:16 -0400

    Add formatted file

---
+++ eslintformatter.js
@@ -0,0 +1,56 @@
+module.exports = function (results, context) {
+  // accumulate the errors and warnings
+
+  // extraction helper func
+  const extractError = () => {
+    const outputObj = {};
+    console.log(results);
+    results.forEach((el) => {
+      el.messages.forEach((mess) => {
+        console.log('mess: ', mess);
+        const keyname = mess.line.toString().concat(':').concat(mess.column.toString());
+
+        console.log(mess);
+        // console.log('mess.line: ', mess.line);
+        // console.log('keyname: ', typeof keyname);
+        // console.log('column: ', typeof mess.column);
+
+        if (!outputObj.keyname) {
+          outputObj[keyname] = mess;
+        }
+      });
+    });
+    console.log('outputObj: ', outputObj);
+    return outputObj;
+  };
+
+  extractError();
+  // only extract eslint by fileId
+
+  const extractFile = () => {
+    // console.log(results);
+    results.forEach((el) => {
+      const { filePath } = el;
+      const file = filePath.split('/');
+      return console.log(`File Path: ${file[file.length - 1]}`);
+    });
+  };
+
+  // extractFile();
+
+  const summary = results.reduce(
+    (seq, current) => {
+      // console.log('RESULT[0].messages:', results[0].messages);
+      seq.errors += current.errorCount;
+      seq.warnings += current.warningCount;
+      return seq;
+    },
+    { errors: 0, warnings: 0 },
+  );
+
+  if (summary.errors > 0 || summary.warnings > 0) {
+    return `Errors: ${summary.errors}, Warnings: ${summary.warnings}\n`;
+  }
+
+  return '';
+};
