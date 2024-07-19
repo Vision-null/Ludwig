8ef2144710e295fc4f48c675ebc455cd59bd3ac9
artmontinski <artmontinski@gmail.com>
2024-07-19 04:32:38 -0700

    correct eslint config and PR guidelines

--- src/logic/logicCompiler.ts
+++ src/logic/logicCompiler.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 import * as Critical from './aria-standards/critical';
 import * as vscode from 'vscode';
 const { JSDOM } = require('jsdom');
-let body: any, document: any;
+let body: any, _document: any;
 
 export interface AriaRecommendations {
   [key: string]: any;
